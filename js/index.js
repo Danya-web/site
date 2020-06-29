2ff4fedca8389e72e78e327bd858465030b30e2d
Danya <bratkodan01@gmail.com>
2020-06-29 16:12:12 +0300

    init

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('.slider').slick({
+        nextArrow: '<button class="slick-arrow slick-next"><img src="./img/next.svg" alt=""></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="./img/prev.svg" alt=""></button>'
+    })
+})
